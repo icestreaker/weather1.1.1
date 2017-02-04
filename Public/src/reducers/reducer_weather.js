83a94b179bd7309fdf506dbd65365e9cc959aa51
James Warren Basham <James Warren Basham>
2017-02-04 15:39:09 -0800

    First Commit

---
+++ Public/src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+
+export default function(state=[], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+//      return state.push(action.payload.data);   //ES5
+    return [ action.payload.data, ...state ];     //ES6
+  }
+  return state;
+}
